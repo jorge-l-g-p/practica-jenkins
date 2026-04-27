206c285f02bdf433e89b15569125b6a0f9be67d4
jorge luis <18853717+jluisgonzalez@users.noreply.github.com>
2026-04-27 11:31:46 -0500

    prueba webhook final

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Hola Jenkins con GitHub desde la u manizales")
 console.log('ejecucion real')
 console.log('prueba automatica funcionando')
-console.log('segundo intento')
+console.log('segundo intento')  
+console.log("prueba en tiempo real")
